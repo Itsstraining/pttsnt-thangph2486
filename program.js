9baa802fcf85a7465fed520d4383491833011435
Thang <tp375883@gmail.com>
2021-01-28 22:58:44 +0700

    thang

--- program.js
+++ program.js
@@ -1,8 +1,47 @@
 // Add new functions, variables here
 
-function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+function main(input) 
+{
+  var prime = function (){
+    var num;
+    for (num = 0; num < 101; num++){
+        if (num % 2 === 0){
+            break;
+        }
+        else if (num % 3 === 0){
+            break;
+        }
+        else if (num % 4=== 0){
+            break;
+        }
+        else if (num % 5 === 0){
+            break;
+        }
+        else if (num % 6 === 0){
+            break;
+        }
+        else if (num % 7 === 0){
+            break;
+        }
+        else if (num % 8 === 0){
+            break;
+        }
+        else if (num % 9 === 0){
+            break;
+        }
+        else if (num % 10 === 0){
+            break;
+        }
+        else if (num % 11 === 0){
+            break;
+        }
+        else if (num % 12 === 0){
+            break;
+        }
+        else {
+            return num;
+        }
+    }
+    }; 
 }
-
 module.exports = main;
